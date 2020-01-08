fae5827d9de4f841bb7e60f2f5675acb5c4ea654
Robert Cicero <robert.cicero@iress.com>
2020-01-08 08:12:27 +0000

    Added creating and retrieving users

--- user.py
+++ user.py
@@ -4,15 +4,13 @@
 
 import decimal
 import json
-from flask import Flask
+from flask import Flask, request, abort
 import aws
 
 app = Flask(__name__)
 
 # Create dynamodb instance
 dynamodb_client = aws.create_dynamodb_client()
-
-user_table = dynamodb_client.table('dynamodb')
 
 
 @app.route('/')
@@ -22,14 +20,32 @@ def hello_world():
 
 @app.route('/createUser')
 def create_user():
-    response = user_table.put_item(
-        Item={
-            'email_address': 'abs@gmail.com',
-            'firstname': 'Robert',
-            'surname': 'Cicero'
-        }
-    )
+    # Argument 1 - email
+    email = request.args.get('email')
+    # Argument 2 - firstname
+    firstname = request.args.get('firstname')
+    # Send item to User table
+    response = dynamodb_client.put_item(TableName='User', Item={
+        'email_address': {'S': email}, 'first_name': {'S': firstname}})
+
     return response
+
+
+@app.route('/getUser')
+def get_user():
+    try:
+        # Argument 1 - email address
+        email = request.args.get('email')
+        # Get data from dynamoDB
+        response = dynamodb_client.get_item(
+            TableName='User', Key={'email_address': {'S': email}})
+        return response
+    except:
+        return custom_400('Email Address not provided or badly formed request.')
+
+
+def custom_400(message):
+    abort(400, description=message)
 
 
 if __name__ == '__main__':
