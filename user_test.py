04e2f224548f150c8101214050547516c2522ab3
Robert Cicero <robert.cicero@iress.com>
2020-01-14 09:04:00 +0000

    Trying to incorporate removing the user from test

--- user_test.py
+++ user_test.py
@@ -20,13 +20,13 @@ class UserTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     # Create test user
-    # def test_valid_user_registration(self):
-    #     response = self.app.post(
-    #         '/createUser',
-    #         data=json.dumps(dict(email='test@NoteIt.com',
-    #                              password='TestPassword123', firstname='Test', surname='TestSurnace')), content_type='application/json'
-    #     )
-    #     self.assertEqual(response.status_code, 200)
+    def test_valid_user_registration(self):
+        response = self.app.post(
+            '/createUser',
+            data=json.dumps(dict(email='test@NoteIt.com',
+                                 password='TestPassword123', firstname='Test', surname='TestSurnace')), content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
 
     # Get Test User
     def test_get_user(self):
@@ -36,6 +36,14 @@ class UserTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         # If email address is found in the return string, then it successfully finds the email
         self.assertGreater(email_start, 0)
+
+    # Remove Test User
+    def test_remove_user(self):
+        response = self.app.post(
+            '/removeUser',
+            data=json.dumps(dict(email='test@NoteIt.com')),
+            content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
