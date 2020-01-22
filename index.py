bd5129e008bf310f65d83120f61eaaa6155db8fc
Robert Cicero <robert.cicero@iress.com>
2020-01-22 18:03:00 +0000

    Added missing dependency

--- index.py
+++ index.py
@@ -5,23 +5,24 @@ from post import create_user
 def lambda_handler(event, context):
 
     # This API is driven off the body containing the attribute 'request_action'
-    #bodydata = json.loads(event['body'])
-    #action = bodydata['request_action']
-    #body = bodydata['data']
+    try:
+        bodydata = json.loads(event['body'])
+        action = bodydata['request_action']
+        body = bodydata['data']
 
-    return {
-        'statusCode': 200,
-        'body': 'Status OK'
-    }
+        return {
+            'statusCode': 200,
+            'body': 'Status OK'
+        }
+    except Exception as identifier:
+        return {
+            'statusCode': 400,
+            'body': 'Body Not formed properly' + str(identifier)
+        }
 
     # if (action == 'Create User'):
     #     result = create_user(body)
     #     return {
     #         'statusCode': result['statusCode'],
     #         'body': result['response']
-    #     }
-    # else:
-    #     return {
-    #         'statusCode': 400,
-    #         'body': 'Incorrect action submitted'
     #     }
