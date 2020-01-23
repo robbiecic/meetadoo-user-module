c2f188fc9439ed340daf4a3e5f613b0c5f6c3ec9
Robert Cicero <robert.cicero.rc@gmail.com>
2020-01-23 21:12:44 +0000

    Adding more API calls

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import json
-from post import create_user
+from post import create_user, remove_user
 
 
 def lambda_handler(event, context):
@@ -9,9 +9,16 @@ def lambda_handler(event, context):
         bodydata = json.loads(event['body'])
         action = bodydata['request_action']
         body = bodydata['data']
+        email = body['email']
 
         if (action == 'Create User'):
             result = create_user(body)
+            return {
+                'statusCode': result['statusCode'],
+                'body': result['response']
+            }
+        elif (action == 'Remove User'):
+            result = remove_user(email)
             return {
                 'statusCode': result['statusCode'],
                 'body': result['response']
--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -5,12 +5,8 @@ import requests
 # Import test data
 with open('tests/create_user.json') as json_file:
     json_data = json.load(json_file)
-    request_action = json_data['request_action']
-    data = json_data['data']
-    email = data['email']
-    firstname = data['firstname']
-    surname = data['surname']
-    password = data['password']
+with open('tests/remove_user.json') as json_file:
+    json_data_remove = json.load(json_file)
 
 # Set URL where API calls are made
 url = "https://i6vtmh1eq3.execute-api.ap-southeast-2.amazonaws.com/Development"
@@ -19,16 +15,10 @@ url = "https://i6vtmh1eq3.execute-api.ap-southeast-2.amazonaws.com/Development"
 class E2ETestCase(unittest.TestCase):
 
     # Remove Test User
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.app.post(
-    #         '/removeUser',
-    #         data=json.dumps(dict(email='test@NoteIt2.com')),
-    #         content_type='application/json')
-    #     cls.app.post(
-    #         '/removeUser',
-    #         data=json.dumps(dict(email='test@NoteIt.com')),
-    #         content_type='application/json')
+    @classmethod
+    def tearDownClass(cls):
+        requests.post(url=url, data=json.dumps(json_data_remove), headers={
+            'content-type': 'application/json'})
 
     # Create test user
     def test_valid_user_registration(self):
