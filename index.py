de2937e21445a5b59a9051a0e12a4f9630f1ab1e
Robert Cicero <robert.cicero.rc@gmail.com>
2020-01-26 16:42:31 +0000

    Added all methods to API

--- index.py
+++ index.py
@@ -1,35 +1,55 @@
 import json
-from post import create_user, remove_user
+from post import create_user, remove_user, login
 
 
 def lambda_handler(event, context):
 
-    # This API is driven off the body containing the attribute 'request_action'
+    # This API is driven off the query string parameter 'request_action'
     try:
+        # For every request, we require a data object containing at least the email
         bodydata = json.loads(event['body'])
         action = event["queryStringParameters"]['action']
         body = bodydata['data']
         email = body['email']
-
-        if (action == 'CreateUser'):
-            result = create_user(body)
-            return {
-                'statusCode': result['statusCode'],
-                'body': result['response']
-            }
-        elif (action == 'RemoveUser'):
-            result = remove_user(email)
-            return {
-                'statusCode': result['statusCode'],
-                'body': result['response']
-            }
-        else:
-            return {
-                'statusCode': 400,
-                'body': "A valid user action was not provided"
-            }
     except Exception as identifier:
         return {
             'statusCode': 400,
             'body': 'Body Not formed properly' + str(identifier)
+        }
+
+    # Enter if statement block to route message
+    if (action == 'CreateUser'):
+        result = create_user(body)
+        return {
+            'statusCode': result['statusCode'],
+            'body': result['response']
+        }
+    elif (action == 'RemoveUser'):
+        result = remove_user(email)
+        return {
+            'statusCode': result['statusCode'],
+            'body': result['response']
+        }
+    elif (action == 'Login'):
+        result = login(body)
+        return {
+            'statusCode': result['statusCode'],
+            'body': result['response']
+        }
+    elif (action == 'isAuthenticated'):
+        result = isAuthenticated(body)
+        return {
+            'statusCode': result['statusCode'],
+            'body': result['response']
+        }
+    elif (action == 'UpdateUser'):
+        result = update_user(body)
+        return {
+            'statusCode': result['statusCode'],
+            'body': result['response']
+        }
+    else:
+        return {
+            'statusCode': 400,
+            'body': "A valid request action was not provided"
         }
