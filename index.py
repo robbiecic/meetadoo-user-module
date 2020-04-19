52b2be5205a8ff9a29508f9f004eb3cfae16e1f5
Robert Cicero <robert.cicero.rc@gmail.com>
2020-04-19 11:10:11 +0100

    Testing semantic versioning

--- index.py
+++ index.py
@@ -1,12 +1,10 @@
 import json
-from user_functions import create_user, remove_user, login, get_user, isAuthenticated, update_user, get_user_list, set_expired_cookie
 from api_handler import API_Handler
 
 
 def lambda_handler(event, context):
     # This API is driven off the query string parameter 'action'
     # Set parameters to pass into class
-    header = {}
     try:
         body = json.loads(event['body'])
     except:
